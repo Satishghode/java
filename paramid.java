ad329d029c8fa9e435917c3810ec29d868f45a81
Satishghode <88142229+Satishghode@users.noreply.github.com>
2022-03-07 05:45:07 -0800

    Update paramid.java

--- paramid.java
+++ paramid.java
@@ -1,6 +1,13 @@
+/*
+        print the pattren.
+             * 
+             *  * 
+             *  *  * 
+             *  *  *  * 
+*/
+
+
 import java.util.*;
-
-
 public class paramid {
     public static void main(String args[]){
         int n = 4;
@@ -8,6 +15,7 @@ public class paramid {
         for(int i = 1 ; i <= n ; i++ ){
             //inner loop 
             for (int j =1 ; j <= i ; j++){
+                //printing the star 
                 System.out.print(" * ");
             }
             System.out.println();
