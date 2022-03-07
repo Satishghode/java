6fc0d4518f4e8d44b31fed0cb911a747c28c5713
Satishghode <88142229+Satishghode@users.noreply.github.com>
2022-03-07 07:42:44 -0800

    Update sort.java

--- sort.java
+++ sort.java
@@ -2,7 +2,7 @@ public class sort {
     public static void main(String[] args) {
         int n= 5 ;
         for (int i=n ;i>=1; i--){
-            //for (int j=n ; j<=i ; j++){
+            for (int j=n ; j<=i ; j++){
                 System.out.print(i+" ");
             //}
             System.out.println();
